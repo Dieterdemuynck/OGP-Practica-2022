eb80f45a5170607ae2324031220fe265142446ab
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2022-02-28 12:07:22 +0100

    File Interface
    Useful for the specification

---
+++ Practicum1/src/com/ogppractica/FileInterface.java
@@ -0,0 +1,11 @@
+package com.ogppractica;
+
+public interface FileInterface {
+
+    public void enlarge(int size);
+
+    public void shorten(int size);
+
+    public boolean hasOverlappingUsePeriod(File other);
+
+}
