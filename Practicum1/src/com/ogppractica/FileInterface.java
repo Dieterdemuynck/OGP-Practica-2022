6a11d7b8e6464fcf95198af1e9474fdaa5656c8e
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-03-02 16:05:53 +0100

    Documentation Enlarge method

--- Practicum1/src/com/ogppractica/FileInterface.java
+++ Practicum1/src/com/ogppractica/FileInterface.java
@@ -1,6 +1,15 @@
 package com.ogppractica;
 
 public interface FileInterface {
+    /**
+     * Enlarges the size of the given file by the given amount of bytes.
+     * @pre     the size is larger than or equal to 0.
+     *       |  size >= 0
+     * @pre     The size cannot be larger than what would exceed the maximum file size, after enlargement.
+     *       |  size <= this.getMaxSize() - this.getSize()
+     * @post    the file size is enlarged by the correct amount of data.
+     * @param size the amount of bytes by which we enlarge the size of the file.
+     */
 
     public void enlarge(int size);
 
