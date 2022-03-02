bd0403f8ce3efac82d24fbe6665b7b364031b05c
Ine Malfait <Ine.malfait@student.kuleuven.be>
2022-03-02 15:39:00 +0100

    Documentatie (versie1) van hasOverlappingUsePeriod(File other)

--- Practicum1/src/com/ogppractica/FileInterface.java
+++ Practicum1/src/com/ogppractica/FileInterface.java
@@ -6,6 +6,12 @@ public interface FileInterface {
 
     public void shorten(int size);
 
+    /**
+     * Indicates whether the usage period of the file overlaps with that of the supplied file. An unmodified file does not overlap with any file.
+     *
+     * @param other the other file
+     * @return True if the usage period overlaps; False if it doesn't overlap or that one of the files is never modified.
+     */
     public boolean hasOverlappingUsePeriod(File other);
 
 }
