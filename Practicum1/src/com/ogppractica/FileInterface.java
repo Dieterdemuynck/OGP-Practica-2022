ea722799573ee8fee4bfe1c5ed847d5e65e283e2
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2022-03-02 15:57:06 +0100

    [Spec] shorten(int) : v1.0

--- Practicum1/src/com/ogppractica/FileInterface.java
+++ Practicum1/src/com/ogppractica/FileInterface.java
@@ -4,6 +4,15 @@ public interface FileInterface {
 
     public void enlarge(int size);
 
+    /**
+     * Shortens the size of the file by the given amount of bytes.
+     *
+     * @pre    the size is larger than or equal to 0.
+     *       | size >= 0
+     * @pre    the size is less than or equal to the current size of the file
+     *       | size <= this.getSize()
+     * @param  size  the amount of bytes by which we shorten the size of the file
+     */
     public void shorten(int size);
 
     /**
