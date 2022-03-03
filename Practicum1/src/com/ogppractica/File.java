582502b0bd1cfec9de0d35e4d0408e1c93e28b21
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2022-03-03 18:29:39 +0100

    [Spec] Class spec + doc cleanup
    Finished class specification and did some cleanup of the documentation by adding and removing newlines and lining up blocks of text using spaces.
    Also added comments indicating what still needs proper specification and what methods the current specification implies have to be created.

--- Practicum1/src/com/ogppractica/File.java
+++ Practicum1/src/com/ogppractica/File.java
@@ -1,6 +1,10 @@
 package com.ogppractica;
 
 public class File implements FileInterface{
+    // TODO: Implement inspector canHaveAsName(String)
+    // TODO: Implement inspector canHaveAsSize(int)
+    // TODO: Implement inspector canHaveAsLastModificationTime(java.util.Date)
+    // TODO: Implement inspector isValidMaxSize(int)
     @Override
     public void enlarge(int size) {
 
--- Practicum1/src/com/ogppractica/FileInterface.java
+++ Practicum1/src/com/ogppractica/FileInterface.java
@@ -1,16 +1,33 @@
 package com.ogppractica;
 
+/**
+ * A class of files involving file name, size, date of creation, date of the latest change or modification, and whether
+ * the file is writable or not. A file is always readable to anyone.
+ *
+ * @invar   file name has at least one character, and all characters must be letters (upper- or lowercase), numbers,
+ *          periods, dashes or underscores.
+ *        | canHaveAsName(getName())
+ * @invar   file size must be smaller than or equal to the maximum size any file is allowed to have.
+ *        | canHaveAsSize(getSize())
+ * @invar   time of last modification must be later than time of creation.
+ *        | canHaveAsLastModificationTime(java.util.Date)
+ * @invar   maximum file size must be positive
+ *        | isValidMaxSize(int)
+ * @author  Team 2: Ine Malfait, Hannes Ingelaere, Dieter Demuynck
+ */
 public interface FileInterface {
+
     /**
      * Enlarges the size of the given file by the given amount of bytes.
-     * @pre     the size is larger than or equal to 0.
-     *       |  size >= 0
-     * @pre     The size cannot be larger than what would exceed the maximum file size, after enlargement.
-     *       |  size <= this.getMaxSize() - this.getSize()
-     * @post    the file size is enlarged by the correct amount of data.
-     * @param size the amount of bytes by which we enlarge the size of the file.
+     *
+     * @pre    the size is larger than or equal to 0.
+     *       | size >= 0
+     * @pre    The size cannot be larger than what would exceed the maximum file size, after enlargement.
+     *       | size <= this.getMaxSize() - this.getSize()
+     * @post   the file size is enlarged by the correct amount of data.
+     *       | TODO: add formal specification
+     * @param  size  the amount of bytes by which we enlarge the size of the file.
      */
-
     public void enlarge(int size);
 
     /**
@@ -18,17 +35,21 @@ public interface FileInterface {
      *
      * @pre    the size is larger than or equal to 0.
      *       | size >= 0
-     * @pre    the size is less than or equal to the current size of the file
+     * @pre    the size is less than or equal to the current size of the file.
      *       | size <= this.getSize()
+     * @post   the file size is shortened by the given amount of data.
+     *       | new.getSize() == this.getSize() - size
      * @param  size  the amount of bytes by which we shorten the size of the file
      */
     public void shorten(int size);
 
     /**
-     * Indicates whether the usage period of the file overlaps with that of the supplied file. An unmodified file does not overlap with any file.
+     * Indicates whether the use period of the file overlaps with that of the given file. An unmodified file does
+     * not overlap with any file.
      *
-     * @param other the other file
-     * @return True if the usage period overlaps; False if it doesn't overlap or that one of the files is never modified.
+     * @param  other  the other file
+     * @return True if the use period overlaps; False if it doesn't overlap or one of the files was never modified.
+     *       | TODO: add formal specification
      */
     public boolean hasOverlappingUsePeriod(File other);
 
