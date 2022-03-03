47523cc475df963caa81ed070e774e1e208d2ef8
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2022-03-03 18:49:55 +0100

    [Repr] representation + getters
    Created the 6 variables needed for the representation, along with the necessary getters.
    The maxSize value doesn't seem to have an automatically generated getters, which I'm curious about. What to do with this?

--- Practicum1/src/com/ogppractica/File.java
+++ Practicum1/src/com/ogppractica/File.java
@@ -1,10 +1,42 @@
 package com.ogppractica;
 
+import java.util.Date;
+
 public class File implements FileInterface{
     // TODO: Implement inspector canHaveAsName(String)
     // TODO: Implement inspector canHaveAsSize(int)
     // TODO: Implement inspector canHaveAsLastModificationTime(java.util.Date)
     // TODO: Implement inspector isValidMaxSize(int)
+
+    private String name;
+    private int size;
+    private final static int maxSize = Integer.MAX_VALUE;  // should this be public?
+    private Date creationTime;
+    private Date lastModificationTime;
+    private boolean isWritable = true;  // file is writable by default
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public Date getLastModificationTime() {
+        return lastModificationTime;
+    }
+
+    public boolean isWritable() {
+        return isWritable;
+    }
+
+    // TODO: Implement steady-state setters for: name, lastModificationTime, isWritable
+
     @Override
     public void enlarge(int size) {
 
