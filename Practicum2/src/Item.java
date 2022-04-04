ed8b333426974c34699b132b76abe50934dff866
Ine Malfait <Ine.malfait@student.kuleuven.be>
2022-04-04 14:27:59 +0200

    Itemklasse -> constructor

--- Practicum2/src/Item.java
+++ Practicum2/src/Item.java
@@ -18,6 +18,36 @@ import java.util.Date;
 public class Item {
 
     /**********************************************************
+     * Constructors
+     **********************************************************/
+
+
+    /**
+     * Initialize a new file with given name, size and writability.
+     *
+     * @param  	name
+     *         	The name of the new file.
+     * @effect  The name of the file is set to the given name.
+     * 			If the given name is not valid, a default name is set.
+     *          | setName(name)
+     * @post    The new creation time of this file is initialized to some time during
+     *          constructor execution.
+     *          | (new.getCreationTime().getTime() >= System.currentTimeMillis()) &&
+     *          | (new.getCreationTime().getTime() <= (new System).currentTimeMillis())
+     * @post    The new file has no time of last modification.
+     *          | new.getModificationTime() == null
+     *
+     * @note	The constructor is annotated raw because at the start of the execution, not all fields are
+     * 			defaulted to a value that is accepted by the invariants.
+     * 			E.g. the name is defaulted to null, which is not allowed,
+     * 			thus the object is in a raw state upon entry of the constructor.
+     */
+    @Raw
+    public Item(String name) {
+        setName(name);
+    }
+
+    /**********************************************************
      * name - total programming
      **********************************************************/
 
