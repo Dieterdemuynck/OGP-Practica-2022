c4d2775ce24806e4b28666652b30916eaa3b56f9
Ine Malfait <Ine.malfait@student.kuleuven.be>
2022-04-05 10:39:18 +0200

    File -> Type

--- Practicum2/src/File.java
+++ Practicum2/src/File.java
@@ -1,7 +1,5 @@
 import be.kuleuven.cs.som.annotate.*;
 
-import java.util.Date;
-
 /**
  * A class of files.
  *
@@ -20,7 +18,7 @@ import java.util.Date;
  * 
  * @note		See Coding Rule 48 for more info on the encapsulation of class invariants.
  */
-public class File extends Item{
+public class File extends Item {
 
     /**********************************************************
      * Constructors
@@ -29,33 +27,29 @@ public class File extends Item{
     /**
      * Initialize a new file with given name, size and writability.
      *
-     * @param  	name
-     *         	The name of the new file.
-     * @param  	size
-     *         	The size of the new file.
-     * @param  	writable
-     *         	The writability of the new file.
-     * @effect  The name of the file is set to the given name.
-     * 			If the given name is not valid, a default name is set.
-     *          | setName(name)
-     * @effect	The size is set to the given size (must be valid)
-     * 			| setSize(size)
-     * @effect	The writability is set to the given flag
-     * 			| setWritable(writable)
-     * @post    The new creation time of this file is initialized to some time during
-     *          constructor execution.
-     *          | (new.getCreationTime().getTime() >= System.currentTimeMillis()) &&
-     *          | (new.getCreationTime().getTime() <= (new System).currentTimeMillis())
-     * @post    The new file has no time of last modification.
-     *          | new.getModificationTime() == null
-     *          
-     * @note	The constructor is annotated raw because at the start of the execution, not all fields are 
-     * 			defaulted to a value that is accepted by the invariants. 
-     * 			E.g. the name is defaulted to null, which is not allowed, 
-     * 			thus the object is in a raw state upon entry of the constructor.
+     * @param name     The name of the new file.
+     * @param size     The size of the new file.
+     * @param writable The writability of the new file.
+     * @effect The name of the file is set to the given name.
+     * If the given name is not valid, a default name is set.
+     * | setName(name)
+     * @effect The size is set to the given size (must be valid)
+     * | setSize(size)
+     * @effect The writability is set to the given flag
+     * | setWritable(writable)
+     * @post The new creation time of this file is initialized to some time during
+     * constructor execution.
+     * | (new.getCreationTime().getTime() >= System.currentTimeMillis()) &&
+     * | (new.getCreationTime().getTime() <= (new System).currentTimeMillis())
+     * @post The new file has no time of last modification.
+     * | new.getModificationTime() == null
+     * @note The constructor is annotated raw because at the start of the execution, not all fields are
+     * defaulted to a value that is accepted by the invariants.
+     * E.g. the name is defaulted to null, which is not allowed,
+     * thus the object is in a raw state upon entry of the constructor.
      */
-	@Raw
-	public File(String name, int size, boolean writable, Type type) {
+    @Raw
+    public File(String name, int size, boolean writable, Type type) {
         super(name);
         setSize(size);
         setWritable(writable);
@@ -65,41 +59,38 @@ public class File extends Item{
     /**
      * Initialize a new file with given name.
      *
-     * @param   name
-     *          The name of the new file.
-     * @effect  This new file is initialized with the given name, a zero size
-     * 			and true writability
-     *         | this(name,0,true)
+     * @param name The name of the new file.
+     * @effect This new file is initialized with the given name, a zero size
+     * and true writability
+     * | this(name,0,true)
      */
-	@Raw
+    @Raw
     public File(String name, Type type) {
-        this(name,0,true,type);
+        this(name, 0, true, type);
     }
 
 
     /**
      * Change the name of this file to the given name.
      *
-     * @param	name
-     * 			The new name for this file.
-     * @effect  The name of this file is set to the given name, 
-     * 			if this is a valid name and the file is writable, 
-     * 			otherwise there is no change.
-     * 			| if (isValidName(name) && isWritable())
-     *          | then setName(name)
-     * @effect  If the name is valid and the file is writable, the modification time 
-     * 			of this file is updated.
-     *          | if (isValidName(name) && isWritable())
-     *          | then setModificationTime()
-     * @throws  FileNotWritableException(this)
-     *          This file is not writable
-     *          | ! isWritable() 
+     * @throws FileNotWritableException(this) This file is not writable
+     *                                        | ! isWritable()
+     * @param    name The new name for this file.
+     * @effect The name of this file is set to the given name,
+     * if this is a valid name and the file is writable,
+     * otherwise there is no change.
+     * | if (isValidName(name) && isWritable())
+     * | then setName(name)
+     * @effect If the name is valid and the file is writable, the modification time
+     * of this file is updated.
+     * | if (isValidName(name) && isWritable())
+     * | then setModificationTime()
      */
     @Override
     public void changeName(String name) throws FileNotWritableException {
         if (isWritable()) {
-            if (isValidName(name)){
-            	super.setName(name);
+            if (isValidName(name)) {
+                super.setName(name);
                 setModificationTime();
             }
         } else {
@@ -107,17 +98,16 @@ public class File extends Item{
         }
     }
 
-    
-    
+
     /**********************************************************
      * size - nominal programming
      **********************************************************/
-    
+
     /**
      * Variable registering the size of this file (in bytes).
      */
     private int size = 0;
-    
+
     /**
      * Variable registering the maximum size of any file (in bytes).
      */
@@ -127,30 +117,32 @@ public class File extends Item{
     /**
      * Return the size of this file (in bytes).
      */
-    @Raw @Basic 
+    @Raw
+    @Basic
     public int getSize() {
         return size;
     }
-    
+
     /**
      * Set the size of this file to the given size.
      *
-     * @param  size
-     *         The new size for this file.
-     * @pre    The given size must be legal.
-     *         | isValidSize(size)
-     * @post   The given size is registered as the size of this file.
-     *         | new.getSize() == size
+     * @param size The new size for this file.
+     * @pre The given size must be legal.
+     * | isValidSize(size)
+     * @post The given size is registered as the size of this file.
+     * | new.getSize() == size
      */
-    @Raw @Model 
+    @Raw
+    @Model
     private void setSize(int size) {
         this.size = size;
     }
-   
+
     /**
      * Return the maximum file size.
      */
-    @Basic @Immutable
+    @Basic
+    @Immutable
     public static int getMaximumSize() {
         return maximumSize;
     }
@@ -158,11 +150,10 @@ public class File extends Item{
     /**
      * Check whether the given size is a valid size for a file.
      *
-     * @param  size
-     *         The size to check.
+     * @param size The size to check.
      * @return True if and only if the given size is positive and does not
-     *         exceed the maximum size.
-     *         | result == ((size >= 0) && (size <= getMaximumSize()))
+     * exceed the maximum size.
+     * | result == ((size >= 0) && (size <= getMaximumSize()))
      */
     public static boolean isValidSize(int size) {
         return ((size >= 0) && (size <= getMaximumSize()));
@@ -171,13 +162,12 @@ public class File extends Item{
     /**
      * Increases the size of this file with the given delta.
      *
-     * @param   delta
-     *          The amount of bytes by which the size of this file
-     *          must be increased.
-     * @pre     The given delta must be strictly positive.
-     *          | delta > 0
-     * @effect  The size of this file is increased with the given delta.
-     *          | changeSize(delta)
+     * @param delta The amount of bytes by which the size of this file
+     *              must be increased.
+     * @pre The given delta must be strictly positive.
+     * | delta > 0
+     * @effect The size of this file is increased with the given delta.
+     * | changeSize(delta)
      */
     public void enlarge(int delta) throws FileNotWritableException {
         changeSize(delta);
@@ -186,13 +176,12 @@ public class File extends Item{
     /**
      * Decreases the size of this file with the given delta.
      *
-     * @param   delta
-     *          The amount of bytes by which the size of this file
-     *          must be decreased.
-     * @pre     The given delta must be strictly positive.
-     *          | delta > 0
-     * @effect  The size of this file is decreased with the given delta.
-     *          | changeSize(-delta)
+     * @param delta The amount of bytes by which the size of this file
+     *              must be decreased.
+     * @pre The given delta must be strictly positive.
+     * | delta > 0
+     * @effect The size of this file is decreased with the given delta.
+     * | changeSize(-delta)
      */
     public void shorten(int delta) throws FileNotWritableException {
         changeSize(-delta);
@@ -201,42 +190,41 @@ public class File extends Item{
     /**
      * Change the size of this file with the given delta.
      *
-     * @param  delta
-     *         The amount of bytes by which the size of this file
-     *         must be increased or decreased.
-     * @pre    The given delta must not be 0
-     *         | delta != 0
+     * @param delta The amount of bytes by which the size of this file
+     *              must be increased or decreased.
+     * @throws FileNotWritableException(this) This file is not writable.
+     *                                        | ! isWritable()
+     * @pre The given delta must not be 0
+     * | delta != 0
      * @effect The size of this file is adapted with the given delta.
-     *         | setSize(getSize()+delta)
+     * | setSize(getSize()+delta)
      * @effect The modification time is updated.
-     *         | setModificationTime()
-     * @throws FileNotWritableException(this)
-     *         This file is not writable.
-     *         | ! isWritable()
+     * | setModificationTime()
      */
-    @Model 
-    private void changeSize(int delta) throws FileNotWritableException{
+    @Model
+    private void changeSize(int delta) throws FileNotWritableException {
         if (isWritable()) {
-            setSize(getSize()+delta);
-            setModificationTime();            
-        }else{
-        	throw new FileNotWritableException(this);
+            setSize(getSize() + delta);
+            setModificationTime();
+        } else {
+            throw new FileNotWritableException(this);
         }
     }
 
     /**********************************************************
      * writable
      **********************************************************/
-   
+
     /**
      * Variable registering whether or not this file is writable.
      */
     private boolean isWritable = true;
-    
+
     /**
      * Check whether this file is writable.
      */
-    @Raw @Basic
+    @Raw
+    @Basic
     public boolean isWritable() {
         return isWritable;
     }
@@ -244,22 +232,34 @@ public class File extends Item{
     /**
      * Set the writability of this file to the given writability.
      *
-     * @param isWritable
-     *        The new writability
-     * @post  The given writability is registered as the new writability
-     *        for this file.
-     *        | new.isWritable() == isWritable
+     * @param isWritable The new writability
+     * @post The given writability is registered as the new writability
+     * for this file.
+     * | new.isWritable() == isWritable
      */
-    @Raw 
+    @Raw
     public void setWritable(boolean isWritable) {
         this.isWritable = isWritable;
     }
 
-
     /**********************************************************
-     * Type and extension
+     * Type
      **********************************************************/
     public final Type type;
 
-    
+    /**
+     * Return the type of this file.
+     */
+    private Type getType(){
+        return type;
+    }
+
+    /**
+     * Return the extension of this file.
+     */
+    private String getExtension() {
+        return type.getExtension();
+    }
 }
+
+
