e99983b4b93b8cc691f8551a3e774670f5e3f59c
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-04-25 16:55:23 +0200

    Get n Setters ingredients

--- Project/src/AlchemicIngredient.java
+++ Project/src/AlchemicIngredient.java
@@ -7,4 +7,44 @@ public class AlchemicIngredient {
     static String state;
     static int quantity;
     static Array temperature;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        AlchemicIngredient.name = name;
+    }
+
+    public static String getIngredientType() {
+        return ingredientType;
+    }
+
+    public static void setIngredientType(String ingredientType) {
+        AlchemicIngredient.ingredientType = ingredientType;
+    }
+
+    public static String getState() {
+        return state;
+    }
+
+    public static void setState(String state) {
+        AlchemicIngredient.state = state;
+    }
+
+    public static int getQuantity() {
+        return quantity;
+    }
+
+    public static void setQuantity(int quantity) {
+        AlchemicIngredient.quantity = quantity;
+    }
+
+    public static Array getTemperature() {
+        return temperature;
+    }
+
+    public static void setTemperature(Array temperature) {
+        AlchemicIngredient.temperature = temperature;
+    }
 }
