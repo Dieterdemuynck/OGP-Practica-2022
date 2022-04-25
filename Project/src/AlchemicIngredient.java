9712892ff5e8de1a70cb06273e7cd1141f908263
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-04-25 16:53:39 +0200

    opnieuw

--- Project/src/AlchemicIngredient.java
+++ Project/src/AlchemicIngredient.java
@@ -1,4 +1,10 @@
-public class Ingredient {
+import java.lang.reflect.Array;
 
-    
+public class AlchemicIngredient {
+
+    static String name;
+    static String ingredientType;
+    static String state;
+    static int quantity;
+    static Array temperature;
 }
