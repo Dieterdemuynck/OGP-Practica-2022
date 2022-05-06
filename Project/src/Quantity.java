4e587ae74605785107524a5f2d81315196be6706
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2022-05-06 16:26:35 +0200

    Minor changes to Quantity, may require big changes soon.

--- Project/src/Quantity.java
+++ Project/src/Quantity.java
@@ -42,7 +42,7 @@ public enum Quantity {
         return value;
     }
 
-    public boolean isCanBeContainer() {
+    public boolean canBeContainer() {
         return canBeContainer;
     }
 
@@ -59,24 +59,58 @@ public enum Quantity {
     }
 
     /**
-     * Returns the amount in the resulting "to" Quantity, given the amount in the given "from" Quantity. This amount,
+     * Returns the equivalent amount in the target Quantity, given the amount in the origin Quantity. This amount,
      * if it is a fraction, will be rounded down.
      *
-     * @param from      The Quantity from which we want to convert
-     * @param to        The Quantity to which we want to convert
-     * @param amount    The amount of product, represented in the "from" Quantity.
-     * @return          The amount of product, represented in the "to" Quantity
+     * @param originQuantity    The Quantity from which we want to convert
+     * @param targetQuantity    The Quantity to which we want to convert
+     * @param amount            The amount of product, represented in the origin Quantity.
+     * @return                  The amount of product, represented in the target Quantity
      */
-    public int convertBetweenQuantities(Quantity from, Quantity to, int amount) {
+    static public int convertBetweenQuantities(Quantity originQuantity, Quantity targetQuantity, int amount) {
         // Will automatically round down:
-        return (int) (amount * from.getValue() / to.getValue());
+        return (int) (amount * originQuantity.getValue() / targetQuantity.getValue());
     }
 
-    public Quantity findLargestFit(State state, int amount) {
-        for(Quantity quantity: EnumSet.allOf(Quantity.class)) {
-            // W.I.P.
-            // Should amount be int or float? Probably float?
+    /**
+     * Returns the equivalent amount in the target Quantity, given the amount in this object's Quantity. This amount,
+     * if it is a fraction, will be rounded down.
+     *
+     * @param targetQuantity    The Quantity to which we want to convert
+     * @param amount            The amount of product, represented in this object's Quantity.
+     * @return                  The amount of product, represented in the target Quantity
+     */
+    public int convertTo(Quantity targetQuantity, int amount) {
+        return convertBetweenQuantities(this, targetQuantity, amount);
+    }
+
+    /**
+     * TODO: due to using floats, fractions will have horrible rounding errors during the runtime of this code,
+     * messing up the entire purpose of some of the lines here. Looking for a solution... This may require
+     * rewriting some of the base Quantity class' code.
+     *
+     * @param state
+     * @param amount
+     * @return
+     */
+    static public Quantity findLargestFit(State state, int amount) {
+        float largestFoundValue = -1;
+        Quantity largestQuantity = null;
+
+        /* Checks *every* quantity, including quantities of other states. Possibly, horribly, inefficient.
+         * T(n) = O(n) with n = amount of defined quantities.
+         * Any ideas to improve this are welcome. An ordered array per state perhaps?
+         * How do we link an array to a state?
+         */
+        for (Quantity quantity: EnumSet.allOf(Quantity.class)) {
+            if (quantity.getRespectiveState() == state || quantity.getRespectiveState() == null) {
+                if (quantity.getValue() > largestFoundValue && amount % quantity.getValue() == 0) {  // % don't work
+                    largestFoundValue = quantity.getValue();
+                    largestQuantity = quantity;
+                }
+            }
         }
-        return null;
+
+        return largestQuantity;
     }
 }
