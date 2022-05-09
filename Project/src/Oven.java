4e8dd8b66cea6886ef102ee4e197bb4fa0af2726
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-05-09 18:05:03 +0200

    fixoven

--- Project/src/Oven.java
+++ Project/src/Oven.java
@@ -10,26 +10,17 @@ public class Oven extends AlchemicDevice { //why do they call it oven when you o
 
     public void setTemperature(long temperature) {
         if (temperature > 0) {
-            this.temperature = temperature;}
+            this.temperature = temperature;
+        }
     }
 
     public void activate() {
         Random rand = new Random();
-        double deviation = rand.nextDouble(0.95,1.05);
-        long tempTemp = Math.round(getTemperature() * deviation);
+        double deviation = rand.nextDouble(0.95, 1.05);
+        long tempTemperature = Math.round(getTemperature() * deviation);
         long[] tempIngredient = getIngredient().getTemperature();
-        long longTempIngredient = getLong(tempIngredient);
-        getIngredient().heat(Math.abs(longTempIngredient - tempTemp));
+        long longTempIngredient = asLong(tempIngredient);
+        getIngredient().heat(Math.abs(longTempIngredient - tempTemperature));
     }
 
-        private long getLong(long[] temperature){
-            if (temperature[0] != 0){
-                return -temperature[0];
-            }
-            else {
-                return temperature[1];
-            }
-        }
-    }
-
-
+}
