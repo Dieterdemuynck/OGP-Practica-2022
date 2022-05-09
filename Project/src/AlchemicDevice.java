076fb4ca0201ee0fc35e4e975c6d66ea7402a7d0
Ine Malfait <Ine.malfait@student.kuleuven.be>
2022-05-09 17:55:25 +0200

    asLong()

--- Project/src/AlchemicDevice.java
+++ Project/src/AlchemicDevice.java
@@ -39,4 +39,13 @@ public abstract class AlchemicDevice {
         return container;
     }
 
+    private long asLong(long[] temperature){
+        if (temperature[0] != 0){
+            return -temperature[0];
+        }
+        else {
+            return temperature[1];
+        }
+    }
+
 }
