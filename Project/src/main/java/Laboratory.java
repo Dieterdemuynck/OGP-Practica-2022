5fbfde92dfbcf4d43b75a8f8f4fabf1df91de802
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-05-15 20:15:16 +0200

    bezig met binary search voor alfabet maar weet nog niet hoe

--- Project/src/main/java/Laboratory.java
+++ Project/src/main/java/Laboratory.java
@@ -1,6 +1,7 @@
 package main.java;
 
 import main.java.device.Device;
+import main.java.ingredient.AlchemicIngredient;
 import main.java.ingredient.IngredientContainer;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import java.util.HashMap;
 public class Laboratory {
 
     private int storeroomCapacity;
-    private HashMap<IngredientContainer, Integer> storage;
+    private ArrayList<AlchemicIngredient> storage;
     private ArrayList<Device> devices;
 
     public int getStoreroomCapacity() {
@@ -20,11 +21,11 @@ public class Laboratory {
         this.storeroomCapacity = storeroomCapacity;
     }
 
-    public HashMap<IngredientContainer, Integer> getStorage() {
+    public ArrayList<AlchemicIngredient> getStorage() {
         return storage;
     }
 
-    public void setStorage(HashMap<IngredientContainer, Integer> storage) {
+    public void setStorage(ArrayList<AlchemicIngredient> storage) {
         this.storage = storage;
     }
 
@@ -45,24 +46,19 @@ public class Laboratory {
         devices.add(device);
     }
 
+   // public int getIndexofIngredient(AlchemicIngredient ingredient) {
+        //int index = storage.size()/2;
+        //storage.get(index)
+
+    //}
+
     //Hier moet er nog rekening worden gehouden met het feit dat verschillende containers verschillend zijn
     // Dimme here: Instead of storing alchemical ingredients, or even just ingredient types, we're storing entire
     // containers? Why... Why containers? Why can we even store the same container multiple times?
     // Are we gonna store an ingredient object 5 times or something and suddenly have 5 times the previous amount out
     // of thin air?
-    public void store(IngredientContainer container, int amount) {
-        if (storage.get(container) == null) {
-            storage.put(container, amount);
-        }
-        else {
-            storage.replace(container, storage.get(container) + amount);
-        }
-        /* TODO: Who in the goddamn heck
-         *  committed a goddamn
-         *  syntax error??
-         * Added " 0 );" so this doesn't break
-         */
-        if (storage.get(container) > 0);
+    public void store(AlchemicIngredient ingredient) {
+
     }
 
 }
