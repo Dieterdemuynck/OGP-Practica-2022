0f36d35ae1ac6212397a1205132c71b8cbf572cb
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-05-16 14:02:00 +0200

    begonnen aan kettle

--- Project/src/main/java/device/Kettle.java
+++ Project/src/main/java/device/Kettle.java
@@ -1,4 +1,28 @@
 package main.java.device;
 
+import main.java.ingredient.AlchemicIngredient;
+
+import java.util.ArrayList;
+
 public class Kettle {  // How did we forget the most important device of them all? I don't have a good feeling about this.
+
+    private ArrayList<AlchemicIngredient> addedIngredients;
+
+    public ArrayList<AlchemicIngredient> getAddedIngredients() {
+        return addedIngredients;
+    }
+
+    public void setAddedIngredients(ArrayList<AlchemicIngredient> addedIngredients) {
+        this.addedIngredients = addedIngredients;
+    }
+
+    public void insert(AlchemicIngredient ingredient) {
+        getAddedIngredients().add(ingredient);
+    }
+
+    public void activate() {
+
+    }
 }
+
+
