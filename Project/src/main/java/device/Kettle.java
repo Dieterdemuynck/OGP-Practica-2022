8b46a118ae90dc12086276e89d42802d89a8ea5b
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-05-18 16:04:38 +0200

    Kettle activate state

--- Project/src/main/java/device/Kettle.java
+++ Project/src/main/java/device/Kettle.java
@@ -43,27 +43,34 @@ public class Kettle {  // How did we forget the most important device of them al
         long[] newTemperature = {0,0};
         String newName = "test";
         State newState = State.Powder;
-//        AlchemicIngredient ingredientTempClosestToRoomTemp = null;
-//        long tempDifference = 20000;
+        AlchemicIngredient ingredientTempClosestToRoomTemp = null;
+        long tempDifference = 20000;
         int i;
         for (i = 0; i == getAddedIngredients().size(); i++) {
             newQuantity += getAddedIngredients().get(i).getQuantityInSpoons(); // TODO: getQuantityInSpoons no work, fix
             long[] tempNewIngredient = getAddedIngredients().get(i).getTemperature();
             if (tempNewIngredient[0] == 0) {
-                newTemperature[1] += tempNewIngredient[1];
-//                if (20 - tempNewIngredient[1] < tempDifference) {
-//                    tempDifference = 20L - tempNewIngredient[1];
-//                    ingredientTempClosestToRoomTemp = getAddedIngredients().get(i);
-//                }
-//              else if ()
-            }
+                newTemperature[1] += tempNewIngredient[1];}
             else if (tempNewIngredient[1] == 0) {newTemperature[0] += tempNewIngredient[0];}
+            long newTempDifference = tempNewIngredient[0] + Math.abs(tempNewIngredient[1] - 20L);
+
+            if (newTempDifference < tempDifference) {
+                tempDifference = newTempDifference;
+                ingredientTempClosestToRoomTemp = getAddedIngredients().get(i);
+            }
+            else if (newTempDifference == tempDifference) {
+                if (getAddedIngredients().get(i).getState() == State.Liquid) {
+                    ingredientTempClosestToRoomTemp = getAddedIngredients().get(i);
+                }
+            }
         }
         newTemperature[0] /= newQuantity;
         newTemperature[1] /= newQuantity;
         if (newTemperature[0] < newTemperature[1]) {newTemperature[1] -= newTemperature[0]; newTemperature[0] = 0;}
 
         else if (newTemperature[1] < newTemperature[0]) {newTemperature[0] -= newTemperature[1]; newTemperature[1] = 0;}
+
+        newState = ingredientTempClosestToRoomTemp.getState();
 
         AlchemicIngredient newIngredient = new AlchemicIngredient(newQuantity, Unit.Spoon, newTemperature, newName, newState );
     }
