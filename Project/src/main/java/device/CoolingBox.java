371187883f0fa259ffb7c457c37f5da761edf8f0
Ine Malfait <Ine.malfait@student.kuleuven.be>
2022-05-19 11:52:12 +0200

    CoolingBox aangepast en IllegalTemperatureException.java is weg want das totaal ;)

--- Project/src/main/java/device/CoolingBox.java
+++ Project/src/main/java/device/CoolingBox.java
@@ -1,13 +1,25 @@
 package main.java.device;
 
+/**
+ * A class of cooling boxes
+ * @author Dieter Demuynck, Hannes Ingelaere and Ine Malfait
+ * @version 1.0
+ */
 public class CoolingBox extends Device {
 
+    /**
+     * Variable registering the temperature of this cooling box
+     */
     private long temperature;
 
     /* *********************************************************
      * DEVICE TYPE
      * *********************************************************/
 
+    /**
+     *
+     * @return
+     */
     @Override
     public DeviceType getDeviceType() {
         return DeviceType.CoolingBox;
@@ -17,24 +29,53 @@ public class CoolingBox extends Device {
      * CONSTRUCTOR TODO
      * *********************************************************/
 
+    /**
+     *
+     */
+    public CoolingBox(){
+        this.temperature = 20;
+    }
 
     /* *********************************************************
      * TEMPERATURE
      * *********************************************************/
+
+    /**
+     *
+     * @return
+     */
     public long getTemperature() {
         // TODO: should return long[] instead
         return temperature;
     }
 
-    public void setTemperature(long temperature) {
-        // TODO: should get a long[] instead
-        this.temperature = temperature;
+    /**
+     *
+     * @param temperature
+     */
+    public void setTemperature(long[] temperature) {
+        // TODO: should get a long[]
+        if (temperature == null) {
+            this.temperature = 20; //DEFAULT
+        }
+        else {
+            long temp = 0;
+            if (temperature[0] != 0) {
+                temp -= temperature[0];
+            } else {
+                temp = temperature[1];
+            }
+            this.temperature = temp;
+        }
     }
 
     /* *********************************************************
      * ACTIVATE
      * *********************************************************/
 
+    /**
+     *
+     */
     public void activate() {
         long tempTemp = getTemperature();
         long[] tempIngredient = getIngredient().getTemperature();
