ccdeacbf83eb6ecf8521a2b1b51e023796f3364a
Hannes Ingelaere <hannes.ingelaere@student.kuleuven.be>
2022-05-19 17:28:25 +0200

    Specificaties bij quantitie en unit, comments bij kettle

--- Project/src/main/java/device/Kettle.java
+++ Project/src/main/java/device/Kettle.java
@@ -6,10 +6,18 @@ import main.java.ingredient.Unit;
 
 import java.util.ArrayList;
 
-public class Kettle extends Device {  // How did we forget the most important device of them all? I don't have a good feeling about this.
+public class Kettle extends Device {
 
+    /**
+     * Variable referencing the total added ingredients, contained in the kettle.
+     */
     private ArrayList<AlchemicIngredient> addedIngredients;
 
+
+    /**
+     * Returns the ingredients contained in the kettle.
+     * @return the ingredients contained in the kettle.
+     */
     public ArrayList<AlchemicIngredient> getAddedIngredients() {
         return addedIngredients;
     }
@@ -34,10 +42,18 @@ public class Kettle extends Device {  // How did we forget the most important de
      * ADDED INGREDIENTS
      * *********************************************************/
 
+    /**
+     * Sets the ingredients contained in the kettle, to a given List of ingredients.
+     * @param addedIngredients ArrayList of the ingredients that will be contained in the kettle.
+     */
     public void setAddedIngredients(ArrayList<AlchemicIngredient> addedIngredients) {
         this.addedIngredients = addedIngredients;
     }
 
+    /**
+     * adds an ingredient to the total added ingredients.
+     * @param ingredient the AlchamicIngredient that will be added to the total added ingredients.
+     */
     public void insert(AlchemicIngredient ingredient) {
         getAddedIngredients().add(ingredient);
     }
@@ -48,21 +64,29 @@ public class Kettle extends Device {  // How did we forget the most important de
      * *********************************************************/
     // TODO:
     //  Mate, start encapsulating more. Please.
+    //  NAME SHOULD HAPPEN AS WELL
+
+    /**
+     * This method activates the kettle, mixing the ingredients contained in the kettle, resulting in a single ingredient.
+     */
     public void activate() {
-        int newQuantity = 0;
+        // The Quantity of the new ingredient, will be the total added quantities of the contained ingredients in spoons.
+        int newQuantity = (int) Math.round(State.addAmountsInSpoons(getAddedIngredients()));
         long[] newTemperature = {0,0};
         String newName = "test";
-        State newState = State.Powder;
         AlchemicIngredient ingredientTempClosestToRoomTemp = null;
         long tempDifference = 20000;
         int i;
         for (i = 0; i == getAddedIngredients().size(); i++) {
-            // TODO: getQuantityInSpoons no longer exists, fix
-            // newQuantity += getAddedIngredients().get(i).getQuantityInSpoons();
             long[] tempNewIngredient = getAddedIngredients().get(i).getTemperature();
-            if (tempNewIngredient[0] == 0) {
-                newTemperature[1] += tempNewIngredient[1];}
+            // adding all the temperatures together, to then, later, calculate the temperature like it's supposed to be.
+            if (tempNewIngredient[0] == 0) {newTemperature[1] += tempNewIngredient[1];}
             else if (tempNewIngredient[1] == 0) {newTemperature[0] += tempNewIngredient[0];}
+
+            // Comparing the difference between the temperature of the relevant ingredient and [0,20],
+            // and the difference of the ingredient with the temperature closest to [0,20], so we can calculate which ingredient to take the State from.
+            // if the relevant ingredient is closer to [0,20], it will become the ingredient with the temperature closest to [0,20]
+            // if the difference is the same, the ingredient with the state = Liquid will have priority over Powder.
             long newTempDifference = tempNewIngredient[0] + Math.abs(tempNewIngredient[1] - 20L);
 
             if (newTempDifference < tempDifference) {
@@ -75,14 +99,16 @@ public class Kettle extends Device {  // How did we forget the most important de
                 }
             }
         }
-        // TODO: was division by zero. after fix, please uncomment, thx :)
-        // newTemperature[0] /= newQuantity;
-        // newTemperature[1] /= newQuantity;
+        // To calculate the new temperature, we calculate the average temperature.
+        newTemperature[0] /= newQuantity;
+        newTemperature[1] /= newQuantity;
+        // Up until now, the temperature has had a long in both indexes. This is illegal, so here, we calculate which index should equal 0,
+        // and the other index will reduce to the right amount.
         if (newTemperature[0] < newTemperature[1]) {newTemperature[1] -= newTemperature[0]; newTemperature[0] = 0;}
 
         else if (newTemperature[1] < newTemperature[0]) {newTemperature[0] -= newTemperature[1]; newTemperature[1] = 0;}
 
-        newState = ingredientTempClosestToRoomTemp.getState();
+        State newState = ingredientTempClosestToRoomTemp.getState();
 
         AlchemicIngredient newIngredient = new AlchemicIngredient(newQuantity, Unit.Spoon, newTemperature, newName, newState );
     }
--- Project/src/main/java/ingredient/Unit.java
+++ Project/src/main/java/ingredient/Unit.java
@@ -1,5 +1,11 @@
 package main.java.ingredient;
 
+/**
+ * A class of units.
+ * @author Dieter Demuynck, Hannes Ingelaere and Ine Malfait
+ * @version 1.0
+ */
+
 public enum Unit {
     // State-independent
     Spoon(true),
@@ -36,6 +42,10 @@ public enum Unit {
 
     private final boolean canBeContainer;
 
+    /**
+     * Returns whether a unit can be a container.
+     * @return boolean of whether the unit can be a container.
+     */
     public boolean canBeContainer() {
         return canBeContainer;
     }
