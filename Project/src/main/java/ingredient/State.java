7b45cbf0afd1b0a1ce7e31025947f7758c32687f
Ine Malfait <Ine.malfait@student.kuleuven.be>
2022-05-20 20:44:04 +0200

    Aanpassing in mixWith (beide versies deze keer ;) )

--- Project/src/main/java/ingredient/State.java
+++ Project/src/main/java/ingredient/State.java
@@ -153,7 +153,7 @@ public enum State {
         return getUnitAt(0);
     }
 
-    public static double toSpoons(int amount, Unit unit) {
+    public double toSpoons(int amount, Unit unit) {
         return (double) amount * getValueOf(unit) / (double) getValueOf(Unit.Spoon);
     }
 
