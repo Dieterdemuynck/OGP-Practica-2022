425a5867bd3d638c254f7a5e25be6db1b1e694c7
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2022-05-20 11:29:28 +0200

    Laboratory.retrieveFromSpecial
    When- when you- when the- you- when- when you wanna get an ingredientcontainer with ingredient based on the special name :)

---
+++ Project/src/main/java/ingredient/exception/SpecialNameDoesNotExistException.java
@@ -0,0 +1,11 @@
+package main.java.ingredient.exception;
+
+import main.java.Laboratory;
+
+// TODO: this exception
+public class SpecialNameDoesNotExistException extends RuntimeException {
+
+    public SpecialNameDoesNotExistException(Laboratory laboratory, String specialName) {
+
+    }
+}
